69ad4c62c7a775681b8346386971bb63b88a4632
udsoncosta <udsoncostasantana@gmail.com>
2023-09-27 11:22:05 -0300

    Criacao da pasta Validator e Classe TemaValidator

---
+++ alemDoSaber/Validator/TemaValidator.cs
@@ -0,0 +1,21 @@
+﻿using RedeSocial;
+using FluentValidation;
+
+namespace RedeSocial.Validator
+{
+    public class TemaValidator : AbstractValidator<Tema>
+    {
+
+        public TemaValidator()
+        {
+            RuleFor(t => t.Descricao)
+                .NotEmpty()
+                .MaximumLength(2000);
+
+            RuleFor(t => t.Assunto)
+               .NotEmpty()
+               .MinimumLenght(5)
+               .MaximumLength(255);
+        }
+    }
+}
