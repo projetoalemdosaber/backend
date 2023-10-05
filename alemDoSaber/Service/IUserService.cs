65e348fc481ef1df9259b1425329c4030b99739b
Robson A. Rocha <robson_it@outlook.com>
2023-10-05 11:22:48 -0300

    Implementação das classes IUserService, IUserService e UserValidator

---
+++ alemDoSaber/Service/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using RedeSocial.Model;
+
+namespace RedeSocial.Service
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User?> GetById(long id);
+        Task<User?> GetByUsuario (string usuario);
+        Task<User?> Create(User usuario);
+        Task<User?> Update(User usuario);
+        //Task Delete (Tema tema);
+
+    }
+}
