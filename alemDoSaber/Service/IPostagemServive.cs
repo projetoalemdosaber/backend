f151f8fd0520af630886c2fd0288099255d57723
udsoncosta <udsoncostasantana@gmail.com>
2023-10-05 11:37:43 -0300

    UserController e User - Login adicionados

--- alemDoSaber/Service/IPostagemServive.cs
+++ alemDoSaber/Service/IPostagemServive.cs
@@ -2,7 +2,7 @@
 
 namespace RedeSocial.Service
 {
-    public interface IPostagemService 
+    public interface IPostagemServive 
     {
         Task<IEnumerable<Postagem>> GetAll();
 
@@ -10,7 +10,7 @@ namespace RedeSocial.Service
 
         Task<IEnumerable<Postagem>> GetByTitulo (string titulo);
 
-        Task<IEnumerable<Postagem>> GetByData (DateTimeOffset dataInicial, DateTimeOffset dataFinal);
+        Task<IEnumerable<Postagem>> GetByData (DateOnly dataInicial, DateOnly dataFinal);
 
         Task<Postagem?> Create(Postagem postagem);
 
